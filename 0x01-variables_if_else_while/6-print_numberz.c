878e803526aa780f02c5545ded4dab256262c734
KBellaBlue <kybfreeman@gmail.com>
2022-09-20 18:21:21 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print 0 to 9
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
