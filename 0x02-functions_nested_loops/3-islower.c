3e1b6b660b470df9276ee8ceee6c5c07f8b3550e
KBellaBlue <kybfreeman@gmail.com>
2022-09-20 19:33:52 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ *
+ * Return: Return 1 if lowercase, otherwise return 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
