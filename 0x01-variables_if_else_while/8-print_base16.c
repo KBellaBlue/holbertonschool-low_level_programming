db4d9bc119b768c3d99d34b726d42c8fcdbd469e
KBellaBlue <kybfreeman@gmail.com>
2022-09-20 18:40:18 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print 0 to 9
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (c = 'a'; c <= 'f'; lette++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
