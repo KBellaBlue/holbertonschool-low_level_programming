3ef8c9f5ceb4ab1ba71087b842bd7551825caf21
KBellaBlue <kybfreeman@gmail.com>
2022-09-20 20:28:18 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * add - add x and y
+ *
+ * @x: the first number
+ * @y: the second number
+ *
+ * Return: x and y added
+ */
+int add(int x, int y)
+{
+	int x;
+	int y;
+
+	return x + y;
+}
