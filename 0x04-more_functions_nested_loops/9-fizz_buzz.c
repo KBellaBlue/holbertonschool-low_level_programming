f79268b620714b4262c5353a3c12945011bd5aa9
KBellaBlue <kybfreeman@gmail.com>
2022-09-26 19:25:48 -0700

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * fizz_buzz - print numbers 1 to 100 but replace multiples of 3 with Fizz, 5 with Buzz, and 15 with FizzBuzz
+ *
+ * @n: the size of the square
+ *
+ * Return: void
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if (x % 3 == 0)
+		{
+			if (x % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+				printf("Fizz");
+			}
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			_putchar(i + 48);
+		}
+		_putchar(' ');
+	}
+	printf("Buzz");
+	_putchar('\n');
+}
