c663b60c575aa12753c0176e8886422c9667a222
KBellaBlue <kybfreeman@gmail.com>
2022-09-26 19:00:33 -0700

    6. The shortest distance between two points is a straight line fixed loop

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,10 @@
-int _putchar(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -6,3 +12,5 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+
+#endif
