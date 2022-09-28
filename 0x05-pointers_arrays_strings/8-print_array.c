0ac6da252ea6da44e6f6a27a8ae6694b2f89adf4
KBellaBlue <kybfreeman@gmail.com>
2022-09-27 18:57:36 -0700

    8. Arrays are not pointers fixed file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * print_array - print a certain number of elements of an array
+ *
+ * @a: the array
+ * @n: the number of elements to print
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+	}
+
+	_putchar('\n');
+}
