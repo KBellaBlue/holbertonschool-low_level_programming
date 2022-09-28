fcb3cbd429960bd859ef21a8e99f64754de8a6a8
KBellaBlue <kybfreeman@gmail.com>
2022-09-27 19:15:54 -0700

    8. Arrays are not pointers fixed file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,15 +16,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
+		printf("%d", a[i]);
 		if (i == (n - 1))
 		{
-			printf("%d", a[n - 1]);
+			continue;
 		}
-		else
-		{
-			printf("%d, ", a[i]);
-		}
+		printf(", ");
 	}
 
-	_putchar('\n');
+	printf("\n");
 }
