d4a0b0d78b1b1b2ba72f19c5d9c8bcd6d9c3990b
KBellaBlue <kybfreeman@gmail.com>
2022-09-27 18:29:16 -0700

    6. Half the lies they tell me about aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * _puts2 - print every other character of a string
+ *
+ * @str:the string
+ *
+ * Return: void
+ */
+void _puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+
+	}
+
+	_putchar('\n');
+}
