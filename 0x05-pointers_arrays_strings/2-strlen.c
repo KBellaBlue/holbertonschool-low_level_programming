8dbc6c5e80eff2b3a41f1d6aea0c898f41c8f966
KBellaBlue <kybfreeman@gmail.com>
2022-09-27 17:30:57 -0700

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -13,7 +13,9 @@ int _strlen(char *s)
 {
 	int length;
 
-	for (length = 0; str[length] != '\0'; length++);
+	for (length = 0; *s[length] != '\0'; length++)
+	{
+	}
 
-	return length;
+	return (length);
 }
