8b95d9025af703ba2a0b5d82a1bef1cdaa71e3ed
KBellaBlue <kybfreeman@gmail.com>
2022-10-02 16:33:37 -0700

    3. strcmp fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,24 +12,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; *(s1 + i) != '\0'; i++)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (*(s2 + i) != '\0')
+		if (*s1 == *s2)
 		{
-			if (*(s1 + i) == *(s2 + i))
-			{
-				continue;
-			}
-			else
-			{
-				break;
-			}
-		}
-		else
-		{
-			break;
+			s1++;
+			s2++;
 		}
 	}
 
