35368d6eed4b027b224ebd94f8a5f9d9ea8a171e
KBellaBlue <kybfreeman@gmail.com>
2022-10-02 16:26:02 -0700

    3. strcpm

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+/**
+ * _strcpm - compare strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if matching otherwise difference
+ */
+int _strcpm(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; i < n && *(s1 + i); i++)
+	{
+		if (*(s1 + i) != '\0' && *(s2 + i) != '0\'){
+			if (*(s1 + i) == *(s2 + i))
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
