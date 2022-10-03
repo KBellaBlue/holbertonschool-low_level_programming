907bed9a3430dd883e51a472b6e9d0123eff1459
KBellaBlue <kybfreeman@gmail.com>
2022-10-03 16:49:41 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * *_memset - fill the memory with a constant byte
+ *
+ * @s: the string 
+ * @b: the constant
+ * @n: the number of bytes
+ *
+ * Return: the string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
