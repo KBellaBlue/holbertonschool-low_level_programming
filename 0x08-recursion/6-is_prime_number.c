746d245473f2b31ad5d7865833b44226dd2ac63e
KBellaBlue <kybfreeman@gmail.com>
2022-10-09 14:27:31 -0700

    6. Inception. Is it possible

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * p2 - 
+ *
+ * @n: the number
+ * @i: prime number
+ *
+ * Return: the square root or -1
+*/
+int p2(int n, int i)
+{
+	if (n == i)
+	{
+		return (1);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	return (p2(n, i + 1));
+}
+/**
+ * is_prime_number - checks for if number is a prime number
+ *
+ * @n: number
+ *
+ * Return: 1 if prime number otherwise 0
+ */
+int is_prime_number(int n)
+{i
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (p2(n, 2));
+}
