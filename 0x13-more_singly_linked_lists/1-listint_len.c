58eadfe324cf99041b07aa35c35eea58a31d007c
KBellaBlue <kybfreeman@gmail.com>
2022-11-08 20:23:50 -0800

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - Returns number of element in list
+ *
+ * @h: The list
+ *
+ * Return: number of nodes
+ *
+ **/
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+		return (0);
+
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+
+	return (i);
+}
