6c82afdfd245b872d2548b144bea4263b8f85f24
KBellaBlue <kybfreeman@gmail.com>
2022-11-09 13:53:45 -0800

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -37,7 +37,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	i = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	i = open(filename, O_RDWR | O_APPEND);
 	if (i < 0)
 		return (-1);
 
@@ -50,7 +50,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	p = write(i, text_content, a);
 
-	if (p < 0 || p != a)
+	if (p < 1 || p != a)
 		return (-1);
 
 	return (1);
