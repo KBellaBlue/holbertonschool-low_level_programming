4a8173d5ab08dab85e4cb05636ce3f78aeb3cda3
KBellaBlue <kybfreeman@gmail.com>
2022-11-08 19:34:37 -0800

    1. 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary version of a number
+ *
+ * @n: number to print
+ *
+ * Return: void
+ *
+ **/
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	if (n >> 1)
+		print_binary(n >> 1);
+
+	i = n & 1;
+	putchar(i + '0');
+}
