31f5693bc7f05b4f6f152d9124afb9a911bcce90
KBellaBlue <kybfreeman@gmail.com>
2022-11-08 16:31:28 -0800

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Prints all elements of a list
+ *
+ * @h: The list
+ *
+ * Return: number of nodes
+ *
+ **/
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a linked list
+ *
+ * @h: The list
+ *
+ * Return: number of nodes
+ *
+ **/
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
