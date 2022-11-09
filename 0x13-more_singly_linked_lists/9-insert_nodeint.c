f235d1b64f0a10184928fc7368c838f691045706
KBellaBlue <kybfreeman@gmail.com>
2022-11-08 21:19:30 -0800

    fixed 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			a = malloc(sizeof(listint_t));
 			if (p == NULL)
+			{
 				return (NULL);
+			}
 			else
 			{
 				a->n = n;
