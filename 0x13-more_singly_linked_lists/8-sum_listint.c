cfb19c7e096838478cecc87539fbc4ceb5405a15
KBellaBlue <kybfreeman@gmail.com>
2022-11-08 20:58:25 -0800

    8. Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all nodes in list
+ *
+ * @head: the starting node of the list
+
+ *
+ * Return: total or 0
+ *
+ **/
+int sum_listint(listint_t *head)
+{
+	listint_t *i;
+	int p;
+
+	if (head == NULL)
+		return (0);
+
+	i = head;
+
+	while (i)
+	{
+		p += i-> n;
+		i = i->next;
+	}
+
+	return (p);
+}
