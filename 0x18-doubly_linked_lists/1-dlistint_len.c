762a92a29dfce026d272851b9fb20ac5a693d0bf
KBellaBlue <kybfreeman@gmail.com>
2022-11-13 12:57:55 -0800

    1. List length

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_len - return number of elements of a list
+ *
+ * @h: the start of the list
+ *
+ * Return: the number of nodes
+ **/
+size_t print_dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		/*printf("%d\n", h->n);*/
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
