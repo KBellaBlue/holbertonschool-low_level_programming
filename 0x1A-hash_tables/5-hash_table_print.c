375498ef16c5125b2b08db4c278ba07a317a67be
KBellaBlue <kybfreeman@gmail.com>
2022-11-14 15:58:56 -0800

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a whole hash table
+ *
+ * @ht: hash table
+ *
+ * Return: void
+ **/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	if (ht->array[i] != NULL)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			if (node != NULL)
+			{
+				printf(" ,");
+			}
+		}
+	}
+	for (i = 1; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+				{
+					printf(" ,");
+				}
+			}
+		}
+	}
+
+	printf("}\n");
+}
