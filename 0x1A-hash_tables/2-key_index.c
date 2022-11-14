92f0d44b16ed48e52c190834deb72a5e379f497e
KBellaBlue <kybfreeman@gmail.com>
2022-11-14 15:30:15 -0800

    2. key -> index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Returns index of a kay
+ *
+ * @key: the key
+ * @size: the size of the table
+ *
+ * Return: the index of the key
+ **/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
